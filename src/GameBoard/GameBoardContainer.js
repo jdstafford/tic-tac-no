67b4146827340861222c9a53fc6a678483878fa9
Josh Stafford <josh@jdstafford.com>
2019-02-03 14:15:11 -0500

    added GameBoard

---
+++ src/GameBoard/GameBoardContainer.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import './GameBoard.css';
+
+class GameBoard extends Component {
+  render() {
+    return <div className="GameBoard" />;
+  }
+}
+
+export default GameBoard;
