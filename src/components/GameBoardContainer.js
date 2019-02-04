1ed9a55581a53362b6af11482d30937179ecc71e
Josh Stafford <josh@jdstafford.com>
2019-02-03 21:14:57 -0500

    added GamePiece component

---
+++ src/components/GameBoardContainer.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class GameBoardContainer extends Component {
+  render() {
+    return <div className="GameBoardContainer" />;
+  }
+}
+
+export default GameBoardContainer;
