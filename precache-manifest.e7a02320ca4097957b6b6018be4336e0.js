b9df9c9a7fddf0e152de72dc9be8c54300d0a43d
Josh Stafford <josh@jdstafford.com>
2019-02-08 21:57:20 -0500

    Updates

---
+++ precache-manifest.e7a02320ca4097957b6b6018be4336e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "26ead58317a649cdf406",
+    "url": "/tic-tac-toe/static/js/runtime~main.26ead583.js"
+  },
+  {
+    "revision": "02972b6758c51d67d839",
+    "url": "/tic-tac-toe/static/js/main.02972b67.chunk.js"
+  },
+  {
+    "revision": "c888a251ce7964d26643",
+    "url": "/tic-tac-toe/static/js/1.c888a251.chunk.js"
+  },
+  {
+    "revision": "02972b6758c51d67d839",
+    "url": "/tic-tac-toe/static/css/main.c41687e2.chunk.css"
+  },
+  {
+    "revision": "778f14409bf9226c468ee09e945fada0",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
